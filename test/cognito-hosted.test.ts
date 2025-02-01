7c123ae456167d4a6bebf5e18ce3b1ec9415eeda
martzcodes <978362+martzcodes@users.noreply.github.com>
2025-01-31 21:37:30 -0500

    Initial commit

---
+++ test/cognito-hosted.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CognitoHosted from '../lib/cognito-hosted-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cognito-hosted-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CognitoHosted.CognitoHostedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
